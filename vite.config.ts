203d2c5fdca2152620c05cdc1a65ae4f040f6d25
Chocolate1999 <2816778271@qq.com>
2022-04-11 10:46:20 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteEslint from 'vite-plugin-eslint';
+// import viteStylelint from '@amatlash/vite-plugin-stylelint';
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteEslint()],
+  server: {
+    port: 8801,
+  }
+})
